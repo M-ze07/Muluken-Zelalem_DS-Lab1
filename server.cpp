948e7aa558e888e889546efea0c6bfcf77f56a78
M-ze07 <119165657+M-ze07@users.noreply.github.com>
2022-12-15 01:03:02 -0900

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -1,6 +1,6 @@
-#include<sys/type>
-#include<unistd> 
-#include<stdio>  
+#include<sys/type> //header file defines a collection of typedef symbols and structures
+#include<unistd> //header defines miscellaneous symbolic constants and types, and declares miscellaneous functions
+#include<stdio>  //header file which has the necessary information to include the input/output related functions in our program
 #include<string>
 #include<stdlib>
 #include<signal>
@@ -36,12 +36,28 @@ int main (){
     { perror ("Error naming channel");
       clean_up(1, &sock);
     }
-  
-  
+ 
    cout<<"Server is a live and waiting for socket connection from client";
     listen(sock, 1);
   
    len = sizeof(serv_name);
   connect_sock = accept(sock, (struct sockaddr *)&serv_name, &len);
+  for (count = 1; count <= SIM_LENGTH; count++)
+    { write(connect_sock, &count, 4);
+      cout<<"Server has written %d to socket." + count;
+  
+  
+  close(connect_sock);  
+  clean_up(0, &sock);
+  
+  return 0;
+}
+  
+//funciton to remove connction
+void clean_up(int cond, int *sock){ 
+  cout<<"Exiting";
+   close(*sock); 
+    exit(cond);
+}
   
   
